4ee49b5e090fa9268d8cbcb0de5403f04346ab00
trunglaca89 <trunglaca89@gmail.com>
2026-03-06 23:45:10 +0700

    Ex6_4.java

---
+++ Homework02/Ex6_4.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArraySortingAndStats{
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+        
+        // Enter Array Size
+        System.out.print("Enter the number of elements in the array: ");
+        int n = keyboard.nextInt();
+        
+        // Int Array
+        int[] my_array = new int[n];
+        
+        // Enter each element
+        for (int i = 0; i < n; i++){
+            my_array[i] = keyboard.nextInt();
+        }
+        
+        System.out.println("\nOriginal array: " + Arrays.toString(my_array));
+        
+        // Sort Array
+        Arrays.sort(my_array);
+        System.out.println("Sorted array: " + Arrays.toString(my_array));
+        
+        // Array Sum
+        int sum = 0;
+        for (int i = 0; i < n; i++){
+            sum += my_array[i];
+        }
+        
+        // Tính trung bình
+        double average = (double) sum / n;
+        
+        System.out.println("Sum of array elements: " + sum);
+        System.out.println("Average of array elements: " + average);
+        
+        keyboard.close();
+    }
+}

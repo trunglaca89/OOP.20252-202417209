4b6a24c6faa143a72cf66418b00c36b4b6f2206e
trunglaca89 <trunglaca89@gmail.com>
2026-03-07 00:07:57 +0700

    Update Ex6_4.java

--- Homework02/Lab01/Ex6_4.java
+++ Homework02/Lab01/Ex6_4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DaysInMonth{
+public class 6_4{
     public static void main(String[] args){
         Scanner keyboard = new Scanner(System.in);
         boolean isValidInput = false;

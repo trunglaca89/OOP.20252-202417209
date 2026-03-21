52a94a91f7099ab6a7e0bdf763948d05508a9a82
trunglaca89 <trunglaca89@gmail.com>
2026-03-21 23:38:20 +0700

    Delete HW04/src/BankAccount.java

--- HW04/src/BankAccount.java
+++
@@ -1,9 +0,0 @@
-public class BankAccount{
-    
-    public static final double MIN_BALANCE = 50000.0;
-    public static final double TRANSFER_FEE_RATE = 0.02;
-
-    private String accountNumber;
-    private String ownerName;
-    private double balance;
-}
